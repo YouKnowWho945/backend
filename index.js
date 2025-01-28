92df1ff27bcf83bf8278aa3f91a58dcd3ebe4408
Jennifer Blalock <cadenblalock@gmail.com>
2025-01-28 13:33:20 -0500

    commited

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+  res.json({message: 'Mwahahahaha'})
+});
+
+app.post('/path', (req, res) => {
+  console.log(req.body);
+  res.json({message: fizzBuzz(req.body.numbers)})
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+function fizzBuzz(numbers) {
+  return numbers;
+}
